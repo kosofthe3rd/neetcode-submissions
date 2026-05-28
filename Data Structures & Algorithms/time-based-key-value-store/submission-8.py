0f62438027412d75d38b0ed292f528d5857de78a
kosofthe3rd <142827230+kosofthe3rd@users.noreply.github.com>
2026-05-28 13:56:42 -0500

    Add: time-based-key-value-store - submission-8

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-8.py
@@ -0,0 +1,32 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+            self.store[key].append([timestamp, value])
+        else:
+            self.store[key].append([timestamp, value])
+        
+
+    
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        print(self.store)
+        if key not in self.store:
+            return ""
+        else:
+            arr = self.store[key]
+            l, r = 0, len(arr) - 1
+
+            while l <= r:
+                m = (l + r) // 2
+
+                if arr[m][0] <= timestamp:
+                    res = arr[m][1]
+                    l = m + 1
+                else:
+                    r = m - 1
+        return res  
