b65c09829e7cd0adbc5a44a6e4b95149add99a43
kosofthe3rd <142827230+kosofthe3rd@users.noreply.github.com>
2026-07-22 20:03:27 -0500

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        dp = [0 for _ in range(len(nums) + 2)]
+        print(dp)
+        print(nums)
+        for i in range(len(nums)-1, -1, -1):
+            skip_next = nums[i] + dp[i + 2]
+            skip_cur = dp[i+1]
+
+            dp[i] = max(skip_cur, skip_next)
+
+        
+        return dp[0]
