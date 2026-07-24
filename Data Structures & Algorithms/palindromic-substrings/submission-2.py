c5616357c8485cef5873e0f6508c409dcc1ecd3a
kosofthe3rd <142827230+kosofthe3rd@users.noreply.github.com>
2026-07-23 20:28:29 -0500

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0 
+
+        def check_pal(l, r):
+            nonlocal res
+            while l >= 0 and r < len(s) and s[r] == s[l]:
+                res += 1
+                l -= 1
+                r += 1
+        
+        for i in range(len(s)):
+            l, r = i, i
+            check_pal(l, r)
+            l, r = i, i + 1
+            check_pal(l, r)
+        
+        return res
